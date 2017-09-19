2cc5166cb03cefe9e2fc71483d83bd23c695ea65
zdl <805504004@qq.com>
2017-09-19 14:40:15 +0800

    Create HelloWord.java File

---
+++ HelloWord.java
@@ -0,0 +1,10 @@
+public class Hello
+{
+	public static void main(String[] args){
+		System.out.printle("Hello Word!!!!!!!");
+		System.out.printle("helloword!!!!");
+		System.out.printle("12315646545");
+		System.out.printle("123123123123123213");
+		System.out.printle("sadlkjasdkljaldkjalskjl");
+	}
+}
